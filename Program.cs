dfa452e2aade7d947ce22f9a134a2b15f88aefa0
Nicole <niconiconi2007@gmail.com>
2024-10-26 21:09:01 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -18,14 +18,21 @@ namespace SortNames
             // Random index numbers for the names
             int xfirstname = random.Next(firstname.Length);
             int xlastname = random.Next(lastname.Length);
+            int middlename = random.Next(lastname.Length);
+            do
+            {
+                middlename = random.Next(lastname.Length);
+            } while (middlename == xlastname);
+            
 
             // Getting the random names
             string fnr = firstname[xfirstname];
             string lnr = lastname[xlastname];
+            string mnr = lastname[middlename];
 
             // result
-            string bothnames = $"{fnr} {lnr}";
-            Console.WriteLine("Random Name: " + bothnames);
+            string allnames = $"{fnr} {lnr} {mnr}";
+            Console.WriteLine("Random Name: " + allnames);
         }
     }
 }
